47a8cd0750ecb09f702756271804827204a54267
Orelbi Acosta <orelbi088@gmail.com>
2022-09-20 01:18:15 -0400

    update

--- src/components/Utils/Config.js
+++ src/components/Utils/Config.js
@@ -1,11 +1,11 @@
 let URL = 'https://chess-server-livid.vercel.app/';
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    // URL = 'http://localhost:5000';
-    URL = 'https://chess-server-livid.vercel.app/';
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+//     // URL = 'http://localhost:5000';
+//     URL = 'https://chess-server-livid.vercel.app/';
     
-} else {
-    URL = '';
-}
+// } else {
+//     URL = '';
+// }
 
 export { URL };
