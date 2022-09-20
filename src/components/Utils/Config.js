09f87c9bd74d1b64c539b12e409e99bbe23cc0a2
Orelbi Acosta <orelbi088@gmail.com>
2022-09-20 03:07:11 -0400

    aaa

--- src/components/Utils/Config.js
+++ src/components/Utils/Config.js
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
+
 let URL = 'https://sore-rose-bullfrog-tutu.cyclic.app';
-=======
->>>>>>> 47d9492acf683cb7970152c39bea0b108affd38a
+
+
 
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
 //     // URL = 'http://localhost:5000';
