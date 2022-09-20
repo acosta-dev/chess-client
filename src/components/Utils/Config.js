215b38bfb4309edfce2a340fefabaef2ddf8da5a
Orelbi Acosta <orelbi088@gmail.com>
2022-09-20 00:30:59 -0400

    Initial

---
+++ src/components/Utils/Config.js
@@ -0,0 +1,11 @@
+let URL = '';
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // URL = 'http://localhost:5000';
+    URL = 'https://chess-server-livid.vercel.app/';
+    
+} else {
+    URL = '';
+}
+
+export { URL };
